71336e00fbd7a39fb86921b0025bc1cc23857460
caopengan <291033240@qq.com>
2020-07-24 11:03:02 +0800

    first commit

---
+++ src/test/java/com/cpa/utils/CommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpa.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
